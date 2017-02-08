360139943eca5dc19d8809af39a7b5f474d9b963
atul-maverick <atulrao12@gmail.com>
2017-02-08 16:40:09 -0600

    automatic earth quake data file read

--- Atul_Konaje_5198_GeoVisualization.py
+++ Atul_Konaje_5198_GeoVisualization.py
@@ -6,10 +6,11 @@ __author__ = 'Atul'
 # import the Bottle framework
 from bottle import static_file
 from bottle import route, template, request,run
+from urllib2  import urlopen
 import json
 import csv
 import json
-
+#http://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
 #Handle the inital request
 @route('/',method="GET")
 def welcome():
@@ -17,34 +18,54 @@ def welcome():
     output = static_file('default.html',root="views/")
     return output
 
+def parse_process_csv(fileopen):
+	reader = csv.reader(fileopen)
+	line=reader.next()
+	fieldnames=line
+	print fieldnames
+	data_dict = csv.DictReader(fileopen,fieldnames)
+	final_value=[]
+	final_value.append(['Lat', 'Long', 'Name','mag'])
+	i=0
+	for row in data_dict:
+		i=i+1
+		if(row['latitude']=="FALSE" or row['longitude']=="FALSE" or row['mag']==""):
+			continue
+	#print(row['latitude'],row['longitude'])
+		try:
+			final_value.append([float(row['latitude']),float(row['longitude']),"Place:"+row['place']+"  Time: "+row['time'],float(row['mag'])])
+		except ValueError:
+			print(row['latitude'],row['longitude'],row['mag'])
+	return final_value
+		
+#	if i==10:
+#		break
+#	print final_value   
+	
 #To recieve the csv file dropped
 @route('/csvdata',method="POST")
 def send():
-
-     fileopen=request.files['file'].file
-     #fname=request.files['file'].filename
-     reader = csv.reader(fileopen)
-     line=reader.next()
-     fieldnames=line
-     print fieldnames
-     data_dict = csv.DictReader(fileopen,fieldnames)
-     final_value=[]
-     final_value.append(['Lat', 'Long', 'Name','mag'])
-     i=0
-     for row in data_dict:
-       i=i+1
-       if(row['latitude']=="FALSE" or row['longitude']=="FALSE"):
-           continue
-       final_value.append([float(row['latitude']),float(row['longitude']),"Place:"+row['place']+"  Time: "+row['time'],float(row['mag'])])
-       #if i==10:
-           #break
-     #print final_value   
-     return json.dumps(final_value)
+	fileopen=request.files['file'].file
+	#fname=request.files['file'].filename
+	final_value=parse_process_csv(fileopen)
+	print(type(final_value))
+	#print(final_value)
+	print(type(json.dumps(final_value)))
+	return json.dumps(final_value)
 
 
-@route('/geochart',"POST")
+@route('/getData/<duration>',method="GET")
+def process(duration):
+	url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv'
+	inpfile=urlopen(url);
+	#inpfile=open("all_month.csv",'rb')
+	final_value=parse_process_csv(inpfile)
+	#print(final_value)
+	return json.dumps(final_value)
+	
 
+@route('/geochart',"POST")	
 def processrequest():
     print "proccessing"
 
-run(host='0.0.0.0', port=8080, debug=True)
+run(host='0.0.0.0', port=3000, debug=True)
